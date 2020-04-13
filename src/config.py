745117952ebe57c1443a38f5509713e91d7ce25b
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-04-13 16:22:00 +0200

    add binary loading

--- src/config.py
+++ src/config.py
@@ -5,5 +5,6 @@ DEV_ITER = 100
 
 cwd = os.getcwd()
 PATH_DATA = os.path.join(cwd,"data","collection")
+PATH_DATA_BIN = os.path.join(cwd,"data","corpus.pkl")
 PATH_STOP_WORDS = os.path.join(cwd, "data", "stop_words.txt")
 PATH_INDEX = os.path.join(cwd, "data", "index.pkl") if DEV_MODE else os.path.join(cwd, "data", "dev_index.pkl")

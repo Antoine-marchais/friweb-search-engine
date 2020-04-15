074c8c1e3635b7f3e2bccd8f76b067e5812ed015
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-04-16 00:50:37 +0200

    add cli in interface

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,11 @@
 from typing import List
+import pickle as pkl
 
 import bool_query as bq
-from preprocess import InvertedIndex
+import vectorial_query as vq
+import argparse
+from preprocess import InvertedIndex, StatCollection
+from config import PATH_INDEX
 
 def retrieve_docs_from_bool_query(query: str, inverted_index: InvertedIndex) -> List[str]:
     lemmatized_query = bq.lemmatize_query(query)
@@ -25,4 +29,30 @@ def retrieve_docs_from_bool_query(query: str, inverted_index: InvertedIndex) -> 
     relevant_documents_id = bq.process_postfix_query(postfix_query, inverted_index.index)
 
     return [inverted_index.mapping[doc_id] for doc_id in relevant_documents_id]
+
+def retrieve_docs_from_vectorial_query(query: str, inverted_index: InvertedIndex, n_results: int) -> List[str]:
+    lemmatized_query = vq.lemmatize_query(query)
+
+    print(lemmatized_query)
+    
+    ids_and_scores = vq.get_scores(lemmatized_query, inverted_index)
+    best_ids_and_scores = sorted(list(ids_and_scores.items()), key=lambda id_and_score: id_and_score[1], reverse=True)[:n_results]
+
+    return [inverted_index.mapping[id_and_score[0]] for id_and_score in best_ids_and_scores]
+
+if __name__ == "__main__" :
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="<boolean|vectorial> model used to process query", default="boolean")
+    parser.add_argument("query", help="query to process")
+    parser.add_argument("--number", "-n", help="number of results (only for boolean)", type=int, default=10)
+    args = parser.parse_args()
+    with open(PATH_INDEX, "rb") as f:
+        inverted_index = pkl.load(f)
+    if args.model == "boolean":
+        print("\n".join(retrieve_docs_from_bool_query(args.query, inverted_index)))
+    elif args.model == "vectorial":
+        print("\n".join(retrieve_docs_from_vectorial_query(args.query, inverted_index, args.number)))
+
+
+    
     

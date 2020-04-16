520514071264c6ec1a470d67a09350d1b80c966a
Antoine-marchais <antoine.marchais@student.ecp.fr>
2020-04-16 15:27:25 +0200

    add tests to vectorial model

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,29 +1,17 @@
 from collections import OrderedDict
 
-from config import PATH_DATA, PATH_STOP_WORDS
-from preprocess import create_corpus_from_files, build_inverted_index, InvertedIndex
+from config import PATH_STOP_WORDS
+from preprocess import build_inverted_index, InvertedIndex
+from mock_data import COLLECTION, INVERTED_INDEX_1, INVERTED_INDEX_2
 
 import pickle
 import pytest
 
 @pytest.mark.xfail(reason="data is not imported in the repo +  pickle doesn't load properly the data")
 def test_build_dev_inverted_index():
-    
-    corpus = create_corpus_from_files(PATH_DATA, dev=True, dev_iter=100)
-    test_index: InvertedIndex = None
+    index_type1 = build_inverted_index(COLLECTION, PATH_STOP_WORDS, type_index=1)
+    assert INVERTED_INDEX_1 == index_type1
 
-    index_type1 = build_inverted_index(corpus, PATH_STOP_WORDS, type_index=1)
-    with open("tests/test_index_type1_100.pkl", "rb") as f:
-        test_index = pickle.load(f)
-    assert test_index == index_type1
-
-    index_type2 = build_inverted_index(corpus, PATH_STOP_WORDS, type_index=2)
-    with open("tests/test_index_type2_100.pkl", "rb") as f:
-        test_index = pickle.load(f)
-    assert test_index == index_type2
-
-    index_type3 = build_inverted_index(corpus, PATH_STOP_WORDS, type_index=3)
-    with open("tests/test_index_type3_100.pkl", "rb") as f:
-        test_index = pickle.load(f)
-    assert test_index == index_type3
+    index_type2 = build_inverted_index(COLLECTION, PATH_STOP_WORDS, type_index=2)
+    assert INVERTED_INDEX_2 == index_type2
 
